e9d7f27ebab4f9c55787c7a6abfc28a29d705d2b
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-02-06 23:40:03 +0530

    UC4-calculated wage for a month

--- EmpWage.js
+++ EmpWage.js
@@ -3,7 +3,11 @@ console.log("==Welcome to the Employee Wage Problem==");
 // UC1- Emp Attendance Present or Absent
 const isFullTime = 1;
 const isPartTime = 2;
-const EmpRatePerHour = 20;
+const empRatePerHour = 20;
+const maxWorkingDays = 20;
+let totalWorkingHrs = 0;
+let empWage = 0;
+let empHrs = 0;
 
 //UC3-Func to get Work hrs
 function getWorkHrs(empCheck) {
@@ -16,9 +20,16 @@ function getWorkHrs(empCheck) {
             return 0;
     }
 }
-//UC2 - calculate Wage
-let empCheck = Math.floor((Math.random() * 10) % 3);
-let empHrs = getWorkHrs(empCheck);
-let empWage = EmpRatePerHour * empHrs;
 
-console.log("Employee Wage is : " + empWage);
+//Uc4- Emp Wage for 20 Working days in a month
+for (let day = 1; day <= maxWorkingDays; day++) {
+    //UC2 - calculate Wage
+    let empCheck = Math.floor((Math.random() * 10) % 3);
+    empHrs = getWorkHrs(empCheck);
+    empWage = empRatePerHour * empHrs;
+    console.log("Emp wage for day " + day + " is : " + empWage);
+    totalWorkingHrs = totalWorkingHrs + empHrs;
+}
+totalEmpWage = empRatePerHour * totalWorkingHrs;
+
+console.log("Employee Wage for 20 Working days is : " + totalEmpWage);
