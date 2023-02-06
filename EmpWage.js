7aa635bc9f3c576e1802b0b72fd4513030a98128
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-02-06 23:25:45 +0530

    UC3-Function Added

--- EmpWage.js
+++ EmpWage.js
@@ -4,17 +4,21 @@ console.log("==Welcome to the Employee Wage Problem==");
 const isFullTime = 1;
 const isPartTime = 2;
 const EmpRatePerHour = 20;
-let empCheck = Math.floor((Math.random()*10)%3);
-switch(empCheck) {
-    case isFullTime:
-        empHrs = 8;
-        break;
-    case isPartTime:
-        empHrs=4;
-        break;
-    default:
-        empHrs=0;
+
+//UC3-Func to get Work hrs
+function getWorkHrs(empCheck) {
+    switch (empCheck) {
+        case isFullTime:
+            return 8;
+        case isPartTime:
+            return 4;
+        default:
+            return 0;
+    }
 }
 //UC2 - calculate Wage
-let empWage = EmpRatePerHour*empHrs;
-console.log("Employee Wage is : "+empWage);
+let empCheck = Math.floor((Math.random() * 10) % 3);
+let empHrs = getWorkHrs(empCheck);
+let empWage = EmpRatePerHour * empHrs;
+
+console.log("Employee Wage is : " + empWage);
